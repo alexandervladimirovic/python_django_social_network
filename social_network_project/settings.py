41ad2b8d2c60f2c6c5af903c3bb7ff8ebad53e97
Alexander <privetworld@gmail.com>
2024-12-08 12:50:57 +0300

    3. Создание API для пользователей (сериализация, вьювсет, роутинг), установка djangorestframework-simplejwt для токенов аутенфикации.

--- social_network_project/settings.py
+++ social_network_project/settings.py
@@ -111,3 +111,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+
+    
+}
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
